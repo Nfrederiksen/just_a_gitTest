657878f083a8c862624f0ea236bf1ee5c722d1f1
Nichollas <nicfr426@student.liu.se>
2020-10-05 15:46:25 +0200

    This is my change

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
+#include <string>
 
 int main() {
 	int apa = 100;
 	std::string str = "korv";
-	std::cout("Welcome to Sweden\n" + apa + "I like falu" + str);
-
+	std::cout << "Welcome to Sweden\n" << apa << "\nI like falu" << str << "\n \n";
 }
 
