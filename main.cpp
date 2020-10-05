ff5e9cf91771e8562e329afde6ecf5911d052c5e
Nfrederiksen <nikko.frederiksen@gmail.com>
2020-10-05 15:57:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main() {
-	int apa2 = 100;
+	int apa2 = 2000;
 	std::string str = "korv";
 	std::cout << "Welcome to Sweden\n" << apa2 << "\nI like falu" << str << "\n \n";
 }
